93e1798f5b9c2a020c8d341bf097e816e912630f
Kirill <borecjeestb@yahoo.com>
2022-06-23 13:36:43 +0500

    proekts

--- Угадайка/test.py
+++ Угадайка/test.py
@@ -1,14 +1,16 @@
-
-from enum import Flag
 from random import *
-x = randint(1, 10)
-while True:
-    y = int(input('угадайте загаданное число от 1 до 100!!!'))
-    if y < x:
-        print('Слишком мало, попробуйте еще раз')
-    elif y > x:
-        print('Слишком много, попробуйте еще раз')
+print('Добро пожаловать в числовую угодайку')
+x = randint(1, 100)
+
+
+def is_valid(f):
+    if f.isdigit():
+        return True
     else:
-        print('Вы угадали, поздравляем!')
-        break
-print('загаданное число - ', x)
+        return False
+
+
+y = is_valid(input('Введите число от 1 до 100 '))
+if y == False:
+    print('Я ведь просил ввести ЧИСЛО от 1 до 100',
+          '\n', 'ладно, введи еще раз')
