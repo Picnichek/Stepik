c5bdf116d2b713bb00c8b69036d57c278117663e
Kirill <borecjeestb@yahoo.com>
2022-06-23 16:45:43 +0500

    угадайка

--- Угадайка/test.py
+++ Угадайка/test.py
@@ -1,16 +1,66 @@
+from ast import While
 from random import *
+from time import process_time
 print('Добро пожаловать в числовую угодайку')
-x = randint(1, 100)
 
 
 def is_valid(f):
-    if f.isdigit():
-        return True
+
+    if f.isdigit() and 0 < int(f):
+        return int(f)
+
     else:
         return False
 
 
-y = is_valid(input('Введите число от 1 до 100 '))
-if y == False:
-    print('Я ведь просил ввести ЧИСЛО от 1 до 100',
-          '\n', 'ладно, введи еще раз')
+pass
+
+
+def test(f):
+    if f < x:
+        f = 'Ваше число меньше загаданного, попробуйте еще разок'
+    elif f > x:
+        f = 'Ваше число больше загаданного, попробуйте еще разок'
+    else:
+        f = 'Вы угадали, поздравляем!'
+    return f
+
+
+pass
+
+
+def communications():
+    count = 0
+    global right_line
+    while True:
+        count += 1
+        print(f'Введите целое число от 1 до {right_line}')
+        y = input()
+        y = is_valid(y)
+        if y == False:
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            continue
+        # if test(y) != 'Вы угадали, поздравляем!':
+        #     continue
+        else:
+            x = test(y)
+            print(x)
+            if x == 'Вы угадали, поздравляем!':
+                print('Количество попыток --', count)
+                break
+
+
+while True:
+    again = input(
+        'Начнем игру? "да" или "нет" ').lower()
+    if again == 'да':
+        right_line = input('задайте границу')
+        right_line = is_valid(right_line)
+        if right_line == False:
+            print('Пожалуйста введите положительное число!!!')
+        else:
+            x = randint(1, right_line)
+            communications()
+    else:
+        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        break
